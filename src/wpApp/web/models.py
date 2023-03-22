ea56888fa1386436995b3574258ac9c50a12dc97
MBatlle6 <mbg29@alumnes.udl.cat>
2023-03-22 15:13:32 +0000

    Apartats 5.1 i 5.2

--- src/wpApp/web/models.py
+++ src/wpApp/web/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=256)
+    def __str__(self) -> str:
+        return self.name
 
 class Project(models.Model):
     name = models.CharField(max_length=256)
+    def __str__(self) -> str:
+        return self.name
 
 class Task(models.Model):
     name = models.CharField(max_length=256)
+    def __str__(self) -> str:
+        return self.name
 
 class Department(models.Model):
     name = models.CharField(max_length=256)
+    def __str__(self) -> str:
+        return self.name
