d62864526b4004f85a9ba2e50d2c8ec95d040429
Faysal Badaoui <faysalbadaoui02@gmail.com>
2023-04-06 12:20:56 +0000

    Models done

--- src/wpApp/web/admin.py
+++ src/wpApp/web/admin.py
@@ -3,8 +3,9 @@ from .models import Company
 from .models import Project
 from .models import Task
 from .models import Department
-
+from .models import User
 admin.site.register(Company)
 admin.site.register(Project)
 admin.site.register(Task)
 admin.site.register(Department)
+admin.site.register(User)
