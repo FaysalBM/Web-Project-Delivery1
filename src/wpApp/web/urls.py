791214151eedb660ace5e693930c84580866b90f
Faysal Badaoui <faysalbadaoui02@gmail.com>
2023-05-08 10:00:37 +0200

    Users now can create new Projects

--- src/wpApp/web/urls.py
+++ src/wpApp/web/urls.py
@@ -7,7 +7,6 @@ from django.urls import path, include
 urlpatterns = [
     path('',views.home, name='web-home'),
     path('about/',views.about, name='web-about'),
-    path('create_project/', views.create_project, name='create_project'),
     path('register/', views.registerPage, name='register_user'),
     path('login/', views.loginPage, name='login_user'),
     path('logout/', views.logoutPage, name='logout_user'),
@@ -15,5 +14,5 @@ urlpatterns = [
     path('department/<int:department_id>/', views.department_detail, name='department_detail'),
     path('add_task/<int:project_id>/', views.add_task, name='add-task'),
     path('delete_task/<int:task_id>/', views.delete_task, name='delete-task'),
-    path('departments/<int:department_id>/create_project/', views.create_project, name='create-project'),
+    path('create_project/<int:department_id>/', views.create_project, name='create-project'),
 ]
