980986e937daab15cc0fdf96cb397a6c5db7e6b5
Faysal Badaoui <faysalbadaoui02@gmail.com>
2023-05-19 21:07:49 +0200

    register and login user tests done

--- src/wpApp/features/steps/authentification.py
+++ src/wpApp/features/steps/authentification.py
@@ -1,48 +1,45 @@
 from behave import given, when, then
 from django.test import Client
-from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-client = Client()
+@given('I navigate to the "{page}" page')
+def step_nav_to_page(context, page):
+    context.browser = Client()
+    context.response = context.browser.get(reverse(page))
 
-@given('I am on the registration page')
-def step_given_on_registration_page(context):
-    response = client.get(reverse('register'))  # Replace 'register' with your register view's URL name
-    assert response.status_code == 200
-
-@when('I register with the following details')
-def step_when_register_with_details(context):
+@when('I enter the following details into the registration form')
+def step_enter_registration_details(context):
     for row in context.table:
-        context.response = client.post(reverse('register'), {  # Replace 'register' with your register view's URL name
+        context.registration_details = {
             'username': row['username'],
-            'password1': row['password'],
-            'password2': row['password'],
-        })
-        assert context.response.status_code == 302
+            'first_name': row['first_name'],
+            'last_name': row['last_name'],
+            'email': row['email'],
+            'password1': row['password1'],
+            'password2': row['password2'],
+        }
 
-@given('Exists a user "{username}" with password "{password}"')
-def step_given_exists_user_with_password(context, username, password):
-    User = get_user_model()
-    User.objects.create_user(username=username, password=password)
+@when('I click the "{button_name}" button')
+def step_click_button(context, button_name):
+    if button_name == 'Register Account':
+        context.response = context.browser.post(reverse('register_user'), context.registration_details)
+    elif button_name == 'Login':
+        context.response = context.browser.post(reverse('login_user'), context.login_details)
 
-@given('I am on the login page')
-def step_given_on_login_page(context):
-    response = client.get(reverse('login'))  # Replace 'login' with your login view's URL name
-    assert response.status_code == 200
-
-@when('I login with username "{username}" and password "{password}"')
-def step_when_login_with_username_and_password(context, username, password):
-    context.response = client.post(reverse('login'), {  # Replace 'login' with your login view's URL name
-        'username': username,
-        'password': password,
-    })
+@then('I should be redirected to the "{page}" page')
+def step_redirect_to_page(context, page):
     assert context.response.status_code == 302
+    assert context.response.url == reverse(page)
 
-@then('I should be redirected to the home page')
-def step_then_redirected_to_home_page(context):
-    assert context.response['Location'] == reverse('home')  # Replace 'home' with your home view's URL name
+@when('I enter the following details into the login form')
+def step_enter_login_details(context):
+    for row in context.table:
+        context.login_details = {
+            'username': row['username'],
+            'password': row['password'],
+        }
 
-@then('I should be logged in as "{username}"')
-def step_then_logged_in_as(context, username):
-    user = get_user_model().objects.get(username=username)
-    assert int(client.session['_auth_user_id']) == user.pk
+@then('I should see a welcome message')
+def step_see_welcome_message(context):
+    response = context.browser.get(context.response.url)
+    assert 'Welcome' in response.content.decode()
